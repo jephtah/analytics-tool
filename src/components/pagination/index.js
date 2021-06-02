2f03d0edcbed6fa12b341c5fb9ef4914ca06bda8
Debayo Tomisin <debayotomisin07@gmail.com>
2021-06-01 18:40:51 -0700

    pagination component complete

---
+++ src/components/pagination/index.js
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+const Pagination = props => {
+  const { hasNext, hasPrevious, forwardClick, backwardClick } = props
+  let content
+  if (hasNext) {
+    content = <button className="mr-6 w-40 flex gap-x-6 justify-center bg-blue-500 py-3 font-bold text-xl" onClick={forwardClick}>Next</button>
+  } else if (hasPrevious) {
+    content = <button className="w-40 flex gap-x-6 justify-center bg-blue-500 py-3 font-bold text-xl" onClick={backwardClick}>Previous</button>
+  } else if (hasNext && hasPrevious) {
+    content = <><button className="mr-6 w-40 flex gap-x-6 justify-center bg-blue-500 py-3 font-bold text-xl" onClick={forwardClick}>Next</button><button className="mr-6 w-40 flex gap-x-6 justify-center bg-blue-500 py-3 font-bold text-xl" onClick={backwardClick}>Previous</button> </>
+  } else {
+    content = null
+  }
+
+  return (
+    <div className="flex mt-4 w-1/2">
+      {content}
+    </div>
+  )
+}
+
+export default Pagination
+
+/* {(hasNext, hasPrevious) => {
+    if (hasNext) {
+    <button className="mr-6 w-40 flex gap-x-6 justify-center bg-blue-500 py-3 font-bold text-xl">Previous</button>
+    } else if (hasPrevious) {
+    <button className="w-40 flex gap-x-6 justify-center bg-blue-500 py-3 font-bold text-xl">Next</button>
+    } else {
+    <>
+        <button className="mr-6 w-40 flex gap-x-6 justify-center bg-blue-500 py-3 font-bold text-xl">Previous</button>
+        <button className="w-40 flex gap-x-6 justify-center bg-blue-500 py-3 font-bold text-xl">Next</button>
+    </>
+    }
+}} */
