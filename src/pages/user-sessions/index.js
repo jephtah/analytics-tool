2f095ef96ce9c24d2f9c1c835b0a4d8787252640
Debayo Tomisin <debayotomisin07@gmail.com>
2021-06-26 12:18:44 -0700

    user sessions and end session complete

--- src/pages/user-sessions/index.js
+++ src/pages/user-sessions/index.js
@@ -16,16 +16,18 @@ const userSession = () => {
 
   useEffect(() => {
     const { query: { username } } = router
-    dispatch(singleSessionActions.getSingleSession('vicsoft'))
+    dispatch(singleSessionActions.getSingleSession(username))
   }, [])
 
   let session
   let activeSessions
   let chatSessions
+  let subscriptionData
   if (singleSession.singleSession) {
-    session = singleSession.singleSession
+    session = singleSession.singleSession || []
     chatSessions = session.length
     activeSessions = session.filter(sess => sess.session_state !== 'inactive').length
+    subscriptionData = []
   }
   console.log(session)
   const sessionsColumns = ['Username', 'Active Session', 'Doctor/Counsellor', 'Date Created', 'End Session']
@@ -51,8 +53,8 @@ const userSession = () => {
                         <div className="flex mt-10">
                             <div className="w-56 h-32 bg-white-300-mobicure rounded-md p-4">
                                 <p className="text-blue-600">Sessions</p>
-                                <p className="mt-3 text-xs"><span className="text-black">Active Sessions:</span>{activeSessions}</p>
-                                <p className="mt-3 text-xs"><span className="text-black">Chat Sessions:</span> {chatSessions}</p>
+                                <p className="mt-3 text-xs"><span className="text-black">Active Sessions:</span>{activeSessions || 'unavailable' }</p>
+                                <p className="mt-3 text-xs"><span className="text-black">Chat Sessions:</span> {chatSessions || 'unavailable' }</p>
                             </div>
                             <div className="w-72 h-32 bg-white-300-mobicure rounded-md ml-12 p-4">
                                 <p className="text-blue-600">Subscription</p>
@@ -62,11 +64,11 @@ const userSession = () => {
                         </div>
                         <div className="mt-16 w-11/12">
                             <p className="text-black text-sm"> Sessions History</p>
-							<table className='border-collapse w-full bg-white rounded-xl'>
+							<table className='border-collapse w-full bg-white rounded-xl mt-4'>
 								<thead>
 									<tr>
 										{ sessionsColumns.map((cols, key) => (
-											<th key={key} className='text-blue-600 text-sm last:text-right p-4 flex-col justify-between'>
+											<th key={key} className='text-blue-600 text-sm text-center p-4 flex-col justify-between last:pr-4 first: pl-4'>
 												{cols}
 											</th>
 										))}
@@ -85,56 +87,58 @@ const userSession = () => {
 									{
 										session.map((row, i) => (
 											<tr key={i}>
-												<td>{row.with_user.username}</td>
-												<td>{row.with_user.verified}</td>
-												<td>{row.with_user.membership_type}</td>
-												<td>{new Date(row.with_user.created_at).toLocaleDateString('en-US')}</td>
+												<td className='px-0 py-4 align-middle text-center transition ease-out delay-300 border-t-2 border-solid border-gray-300'>{row.with_user.username}</td>
+												<td className='px-0 py-4 align-middle text-center transition ease-out delay-300 border-t-2 border-solid border-gray-300'>{row.with_user.verified}</td>
+												<td className='px-0 py-4 align-middle text-center transition ease-out delay-300 border-t-2 border-solid border-gray-300'>{row.with_user.membership_type}</td>
+												<td className='px-0 py-4 align-middle text-center transition ease-out delay-300 border-t-2 border-solid border-gray-300'>{new Date(row.with_user.created_at).toLocaleDateString('en-US')}</td>
 												{row.session_state !== 'inactive'
-												  ? (<td> <Switch onChange = {() => handleChange(row.with_user, row.with_user.id)} /> </td>)
-												  : (<td><Switch disabled checked/></td>)
+												  ? (<td className='px-0 py-4 align-middle text-center pr-4 transition ease-out delay-300 border-t-2 border-solid border-gray-300'> <Switch onChange = {() => handleChange(row.with_user, row.with_user.id)} /> </td>)
+												  : (<td className='px-0 py-4 align-middle text-center pr-4 transition ease-out delay-300 border-t-2 border-solid border-gray-300'><Switch disabled checked/></td>)
 												}
 											</tr>
 										))
 									}
 								</tbody>
 							</table>
-                            <div className="h-auto bg-white-300-mobicure mt-4 rounded-md p-4 flex justify-between">
-                                <div>
-                                    <span className="text-blue-600 text-sm">Username</span>
-                                </div>
-                                <div>
-                                    <span className="text-blue-600 text-sm">Active Session</span>
-                                </div>
-                                <div>
-                                    <span className="text-blue-600 text-sm">Doctor/Counsellor</span>
-                                </div>
-                                <div>
-                                    <span className="text-blue-600 text-sm">Date Created</span>
-                                </div>
-                                <div>
-                                    <span className="text-blue-600 text-sm">End Session</span>
-                                </div>
-                            </div>
                         </div>
                         <div className="mt-16 w-11/12">
                             <div className="flex justify-between">
                                 <p className="text-black text-sm"> Subscriptions History</p>
                                 <p className="text-green-600 text-sm"> <span className="text-gray-400">Sort By</span> Week </p>
                             </div>
-                            <div className="h-auto bg-white-300-mobicure mt-4 rounded-md p-4 flex justify-between">
-                                <div>
-                                    <span className="text-blue-600 text-sm">Subscription Type</span>
-                                </div>
-                                <div>
-                                    <span className="text-blue-600 text-sm">Subscription Amount</span>
-                                </div>
-                                <div>
-                                    <span className="text-blue-600 text-sm">Subscription Left</span>
-                                </div>
-                                <div>
-                                    <span className="text-blue-600 text-sm">Purchase Date</span>
-                                </div>
-                            </div>
+                            <table className='border-collapse w-full bg-white rounded-xl mt-4'>
+								<thead>
+									<tr>
+										{ SubscriptionColumns.map((cols, key) => (
+											<th key={key} className='text-blue-600 text-sm text-center p-4 flex-col justify-between last:pr-4 first: pl-4'>
+												{cols}
+											</th>
+										))}
+									</tr>
+								</thead>
+								<tbody>
+									{
+										!subscriptionData.length && (
+											<tr>
+												<td colSpan={sessionsColumns.length}>
+													<em className='block text-red-400 p-4 w-full text-center'>no data</em>
+												</td>
+											</tr>
+										)
+									}
+									{
+										subscriptionData.map((row, i) => (
+											<tr key={i}>
+												<td className='px-0 py-4 align-middle text-center transition ease-out delay-300 border-t-2 border-solid border-gray-300'>{row.subscriptiontype}</td>
+												<td className='px-0 py-4 align-middle text-center transition ease-out delay-300 border-t-2 border-solid border-gray-300'>{row.subscriptionAmount}</td>
+												<td className='px-0 py-4 align-middle text-center transition ease-out delay-300 border-t-2 border-solid border-gray-300'>{row.subscriptionLeft}</td>
+												<td className='px-0 py-4 align-middle text-center transition ease-out delay-300 border-t-2 border-solid border-gray-300'>{new Date(row.with_user.created_at).toLocaleDateString('en-US')}</td>
+												<td className='px-0 py-4 align-middle text-center pr-4 transition ease-out delay-300 border-t-2 border-solid border-gray-300'>{row.purchaseDate}</td>
+											</tr>
+										))
+									}
+								</tbody>
+							</table>
                         </div>
                     </>
                   : <div>Loading...</div>
